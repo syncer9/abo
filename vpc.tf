a8c070716508d704e7858d4a93d4242b284ca64c
jkkang <syncer9@gmail.com>
2021-12-07 11:42:27 +0900

    2

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "example" {
-  cidr_block = "172.10.0.0/20"
+  cidr_block = "172.16.0.0/16"
   tags = {
     Name = "example"
   }
@@ -7,12 +7,12 @@ resource "aws_vpc" "example" {
 
 resource "aws_subnet" "example-a" {
   vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.10.0.0/24"
+  cidr_block = "172.16.0.0/24"
   availability_zone = "ap-northeast-2a"
 }
 
 resource "aws_subnet" "example-c" {
   vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.10.1.0/24"
+  cidr_block = "172.16.1.0/24"
   availability_zone = "ap-northeast-2c"
 }
