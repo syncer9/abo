96810aa34447dd7d3cc20a23b71c5127c4a7a579
syncer9 <syncer9@gmail.com>
2021-12-12 16:24:10 +0900

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -11,13 +11,6 @@ resource "aws_security_group" "example-allow-all" {
   }
 
 
-  ingress {
-    from_port = 22
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port = 0
     to_port = 0
